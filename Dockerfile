f95f613205601d3ba74045974d50c7afdb8dc69b
Ahmad Husen <hapakaien@gmail.com>
2021-05-18 16:49:00 +0700

    feat: add basic CI

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,17 @@
-FROM alpine:3.10
+# Base image
+FROM docker.io/library/archlinux:base-devel
 
+# Install dependencies
+RUN pacman -Syu --needed --noconfirm pacman-contrib namcap
+
+# Setup user
+RUN useradd -m builder \
+    usermod -aG wheel builder \
+    echo 'wheel  ALL=(ALL:ALL) ALL' >> /etc/sudoers
+
+# Copy files
 COPY LICENSE README.md /
-
 COPY entrypoint.sh /entrypoint.sh
 
+# Set entrypoint
 ENTRYPOINT ["/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,23 @@
 #!/bin/sh -l
 
-echo "hello $1"
+cd $INPUT_PATH
+
+if [[ $INPUT_UPDPKGSUMS == true ]]; then
+  echo "Update checksums on PKGBUILD"
+  updpkgsums
+  git diff PKGBUILD
+fi
+
+if [[ $INPUT_SRCINFO == true ]]; then
+  echo "Generate new .SRCINFO based on PKGBUILD"
+  sudo -u builder makepkg --printsrcinfo > .SRCINFO
+  git diff .SRCINFO
+fi
+
+echo "Run makepkg with flags"
+sudo -u builder makepkg $INPUT_FLAGS
+
+if [[ $INPUT_NAMCAP == true ]]; then
+  echo "Validate PKGBUILD with namcap"
+  namcap -i PKGBUILD
+fi
