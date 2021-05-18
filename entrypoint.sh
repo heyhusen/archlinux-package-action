444f56b20f7a956ec69123c6c8c481fbc68c626e
Ahmad Husen <hapakaien@gmail.com>
2021-05-18 20:46:48 +0700

    feat: use default path, lint bash script & show diff

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,24 +1,26 @@
 #!/bin/sh -l
 
-sudo chown -R builder $GITHUB_WORKSPACE
+sudo chown -R builder $INPUT_PATH
 cd $INPUT_PATH
 
 if [[ $INPUT_UPDPKGSUMS == true ]]; then
-  echo "Update checksums on PKGBUILD"
-  sudo -u builder updpkgsums
-  git diff PKGBUILD
+    echo "Update checksums on PKGBUILD"
+    sudo -u builder updpkgsums
+    git diff PKGBUILD
 fi
 
 if [[ $INPUT_SRCINFO == true ]]; then
-  echo "Generate new .SRCINFO based on PKGBUILD"
-  sudo -u builder makepkg --printsrcinfo > .SRCINFO
-  git diff .SRCINFO
+    echo "Generate new .SRCINFO based on PKGBUILD"
+    sudo -u builder makepkg --printsrcinfo > .SRCINFO
+    git diff .SRCINFO
 fi
 
-echo "Run makepkg with flags"
-sudo -u builder makepkg $INPUT_FLAGS
+if [[ -n "$INPUT_FLAGS" ]]; then
+    echo "Run makepkg with flags"
+    sudo -u builder makepkg $INPUT_FLAGS
+fi
 
 if [[ $INPUT_NAMCAP == true ]]; then
-  echo "Validate PKGBUILD with namcap"
-  namcap -i PKGBUILD
+    echo "Validate PKGBUILD with namcap"
+    namcap -i PKGBUILD
 fi
