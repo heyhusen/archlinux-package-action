7f32fc286b007d57afb814bef7943bc861f3476f
Ahmad Husen <hi@husen.id>
2021-05-30 14:00:48 +0700

    feat(container): only copy needed files

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
 # Set path
-echo '::group::Changing directory to $PATH'
-cd ./$INPUT_PATH
+echo '::group::Copying file from $WORKPATH to /tmp/gh-action'
+WORKPATH=./$INPUT_PATH
+# Set path permision
+sudo -u builder cp -fv $WORKPATH/PKGBUILD /tmp/gh-action
+cd /tmp/gh-action
 echo '::endgroup::'
 
 # Update checksums
 echo '::group::Updating checksums on PKGBUILD'
 if [[ $INPUT_UPDPKGSUMS == true ]]; then
     sudo -u builder updpkgsums
-    sudo -u builder git diff PKGBUILD
+    git diff PKGBUILD
 fi
 echo '::endgroup::'
 
@@ -17,14 +20,14 @@ echo '::endgroup::'
 echo '::group::Generating new .SRCINFO based on PKGBUILD'
 if [[ $INPUT_SRCINFO == true ]]; then
     sudo -u builder makepkg --printsrcinfo > .SRCINFO
-    sudo -u builder git diff .SRCINFO
+    git diff .SRCINFO
 fi
 echo '::endgroup::'
 
 # Validate with namcap
 echo '::group::Validating PKGBUILD with namcap'
 if [[ $INPUT_NAMCAP == true ]]; then
-    sudo -u builder namcap -i PKGBUILD
+    namcap -i PKGBUILD
 fi
 echo '::endgroup::'
 
@@ -33,4 +36,9 @@ echo '::group::Running makepkg with flags'
 if [[ -n "$INPUT_FLAGS" ]]; then
     sudo -u builder makepkg $INPUT_FLAGS
 fi
+echo '::endgroup::'
+
+echo '::group::Copying files from /tmp/gh-action to $WORKPATH'
+cp -fv /tmp/gh-action/PKGBUILD $WORKPATH/PKGBUILD
+cp -fv /tmp/gh-action/.SRCINFO $WORKPATH/.SRCINFO
 echo '::endgroup::'
